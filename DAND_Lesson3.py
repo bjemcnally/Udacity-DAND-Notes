4785ba09f0d5cc11361c99cd93fa27b165d8460b
Joy McNally <bjaerickson@gmail.com>
2018-03-05 12:27:15 -0800

    Lesson 3 notes 3/5/2018
    started Lesson 3 today

---
+++ DAND_Lesson3.py
@@ -0,0 +1,54 @@
+# CONTROL FLOW: INTRODUCTION
+
+'''
+conditional statements and loops
+    Boolean expressions
+    for and while loops
+    break and continue to exit or skip loops
+    helpful built in functions (e.x. zip and enumerate)
+    list comprehensions
+'''
+
+# CONDITIONAL STATEMENTS
+
+'''
+if statements run or skip indented code based upon whether a statement is TRUE or FALSE 
+'''
+
+if phone_balace < 5:
+        phone_balance += 10
+        bank_balance -=
+
+'''
+if, elif, and else
+
+elif evaluates if previous if is false. you can have multiple elif blocks to handle different situations
+
+indentation, multiples of four (4) spaces
+'''
+
+# Quiz
+
+points = 174  # use this input when submitting your answer
+
+# write your if statement here
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dea, no prize this time."
+elif points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+elif prize <= 200:
+    result = "Congratulations! You won a pengiun!"
+
+print(result)
+
+'''
+Note: Lower bounds of comparison operators are not required because previous statement would apply if points was <50, etc.
+'''
+
+# Boolean Expressions for Conditions
+
+'''
+
+'''
